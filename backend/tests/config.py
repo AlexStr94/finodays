e6fbe9da05cc0b69772d458bfe4aeb1193a0ede6
AlexStr94 <alex.strunskiy@ya.ru>
2023-10-02 11:47:43 +0300

    Подправил фиксатуру с тестовым клиентом

--- backend/tests/config.py
+++ backend/tests/config.py
@@ -11,10 +11,6 @@ SQLALCHEMY_DATABASE_URL = 'postgresql+asyncpg://test:test@localhost:5432/test'
 async def override_get_session() -> AsyncSession:
     engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True, future=True)
 
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
-
     async_session = sessionmaker(
         engine, class_=AsyncSession, expire_on_commit=False
     )
@@ -23,3 +19,12 @@ async def override_get_session() -> AsyncSession:
 
 
 app.dependency_overrides[get_session] = override_get_session
+
+
+@app.on_event("startup")
+async def startup_event():
+    engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True, future=True)
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
