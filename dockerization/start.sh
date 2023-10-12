6fc55d82b5388d79ca2aa13703849f53c9ac1fb9
AlexStr94 <alex.strunskiy@ya.ru>
2023-10-12 20:47:21 +0300

    2 workers

--- dockerization/start.sh
+++ dockerization/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 alembic upgrade head
-gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --timeout-keep-alive 30
+gunicorn main:app --workers 2 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --timeout-keep-alive 30
