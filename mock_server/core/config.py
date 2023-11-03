a3dff1117652a825e85865d72fd8da359c76f1d6
AlexStr94 <alex.strunskiy@ya.ru>
2023-11-03 15:36:14 +0300

    убрал лог транзакций в мок сервере

--- mock_server/core/config.py
+++ mock_server/core/config.py
@@ -20,7 +20,7 @@ class AppSettings(BaseSettings):
         'SECRET_KEY',
         'a8e65ae1f66694950808f5318419a729c8576d2275f823edde47f7cc36a26d51'
     )
-    echo: bool = True #(os.getenv('ECHO', 'True') == 'True')
+    echo: bool = os.getenv('ECHO', 'False') == 'True'
     
 
     class Config:
